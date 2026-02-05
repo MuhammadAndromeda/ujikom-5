ce9d203efabeeb7f68ee459f716c6d595c8b3325
andromeda <q.a.r.h.hrp0204@gmail.com>
2026-02-05 09:19:30 +0700

    Filament Installation

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,1 +1,4 @@
-<h1>Ini Navbar</h1>
+<div class="w-full px-20 flex justify-between items-center">
+    <h1>Ini Navbar</h1>
+    <a href="/admin/login" class="py-3 px-8 rounded-sm bg-cyan-900 text-white">Login</a>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home', ['title' => 'Home']);
 });
+
+Route::get('/about', function () {
+    return view('about', ['title' => 'About']);
+});
